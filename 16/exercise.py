b6bf2ff3e601a6f590c9830c5f2998639ee4831c
Juhana Autti <ext-juhana.autti@3stepit.com>
2022-12-16 16:04:49 +0200

    Day 16 fix floydMarshall, add debug prints

--- 16/exercise.py
+++ 16/exercise.py
@@ -9,26 +9,33 @@ from functools import lru_cache
 from itertools import chain
 
 def floydWarshall(targets):
-	dist = [[999999 for i in range(len(targets))] for j in range(len(targets))]
+	dist = [[99 for i in range(len(targets))] for j in range(len(targets))]
 	for vi, targs in enumerate(targets):
 		for target in targs:
 			dist[vi][target] = 1
 		dist[vi][vi] = 0
-	for k in range(1, len(targets)):
-		for i in range(1, len(targets)):		
-			for j in range(1, len(targets)):
+	for k in range(len(targets)):
+		for i in range(len(targets)):		
+			for j in range(len(targets)):
 				if dist[i][j] > dist[i][k] + dist[k][j]:
 					dist[i][j] = dist[i][k] + dist[k][j]
 	return dist
 
 def exercise(arr):
 	ind = { a: i for i, (a, b, c) in enumerate(arr)}
+	og = [a for (a, b, c) in arr]
 	targets = [[ind[ci] for ci in c] for a, b, c in arr]
 	pressures = [int(b) for a, b, c in arr]
 
 	dist = floydWarshall(targets)
 	start = ind['AA']
 	l = tuple(filter(lambda x: pressures[x] > 0, map(lambda x: x[0], enumerate(pressures))))
+
+	# print(", ".join([og[i] for i in range(10)]))
+	# for i, row in enumerate(dist):
+	# 	print(", ".join([str(val).rjust(2,"0") for val in row]), end=" ")
+	# 	print(og[i])
+	# print("non-zero:", [og[i] for i in l])
 
 	def chooseOne(xl):
 		for i in range(len(xl)):
@@ -53,18 +60,17 @@ def exercise(arr):
 	def dfs2(cur, rest, time):
 		return max(gen2(cur, rest, time), default=dfs(start, rest, 26))
 
-
 	@timer
 	@print_result
 	def exercise1():
 		return dfs(start, l, 30)
-	exercise1()
+	exercise1() # 1651 / 1944
 
 	@timer
 	@print_result
 	def exercise2():
 		return dfs2(start, l, 26)
-	exercise2()
+	exercise2() # 1707 / 2679
 
 def main(args=None):
 	input = init(path.dirname(__file__), inputs.read_to_str_arr, args)
