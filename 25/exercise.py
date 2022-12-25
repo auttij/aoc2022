7574fca881676a659c6ae31347a197699b3c26ce
Juhana Autti <ext-juhana.autti@3stepit.com>
2022-12-25 07:44:14 +0200

    Add day 25

---
+++ 25/exercise.py
@@ -0,0 +1,44 @@
+import logging
+from sys import argv
+from os import path
+from aocHelpers import inputs
+from aocHelpers.decorators import timer, print_result
+from aocHelpers.init import init
+
+def fromSnafu(num):
+	a = [n.replace('-', '-1').replace('=', '-2') for n in list(num)]
+	b = [int(n, 5) * pow(5, i) for i, n in enumerate(a[::-1])]
+	return sum(b)
+
+def toSnafu(num):
+	b5 = []
+	while num:
+		b5.append(num % 5)
+		num //= 5
+
+	b5.reverse()
+	while any(n >= 3 for n in b5):
+		for i in range(len(b5)):
+			if b5[i] >= 3:
+				b5[i-1] += 1
+				b5[i] -= 5
+	return "".join(["0","1","2","=","-"][n] for n in b5)
+
+@timer
+@print_result
+def exercise1(arr):
+	ans = sum((fromSnafu(i) for i in arr))
+	return toSnafu(ans)
+
+@timer
+@print_result
+def exercise2(arr):
+	pass
+
+def main(args=None):
+	arr = init(path.dirname(__file__), inputs.read_to_str_arr, args)
+	exercise1(arr.copy())
+	exercise2(arr.copy())
+
+if __name__ == "__main__":
+	main(argv[1:])
